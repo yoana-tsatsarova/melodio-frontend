82fc82ae7f52f7ad6e6d1e8a7bb480640bcc75a8
yoana-tsatsarova <yoana.tsatsarovau@appliedtechnology.se>
2023-07-18 10:50:03 +0200

    got playlist id

--- app/something/page.tsx
+++ app/something/page.tsx
@@ -4,26 +4,38 @@ import axios from "axios";
 
 const Page = () => {
 
+    const accessToken = 'BQBxR0XaPin2uOgID_33uw2uz39_kKLfe5tlSxKtthdYwSCiqro6ubXrLZcRhAAwmV061Jetl18_FqQzuhEDVMzOZvoOUeo6f5wPNQrinddZepuIL5c'
+
     const [response, setResponse] = useState('');
 
-    useEffect(()=> {
+    const [playlistID, setPlaylistID] = useState('');
+
+    useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('https://journeo.azurewebsites.net/');
-                setResponse(response.data);
-                console.log(response);
+                const url = 'https://api.spotify.com/v1/search?q=Viral+50+Netherlands&type=playlist&market=NL&limit=1';
+                const headers = {
+                    Authorization: `Bearer ${accessToken}`,
+                };
+
+
+                const response = await axios.get(url, { headers });
+                console.log(response.data.playlists.items[0].id);
+
+                setResponse(response);
             } catch (error) {
                 console.error(error);
-            }}
+            }
+        };
 
         fetchData();
+    }, []);
 
-    }, [])
 
 
 
     return (
-        <div className={"text-6xl text-red-600 "}>{response}</div>
+        <div className={"text-6xl text-red-600 "}>{response.length}</div>
     )
 }
 export default Page
