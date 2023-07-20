a4c8abe390ca85f37b139a2ac6bed6dbe099156d
“Carolina <carolinanichita89@gmail.com>
2023-07-20 15:44:44 +0200

    deleting song from playlist

---
+++ app/favorites/page.tsx
@@ -0,0 +1,70 @@
+"use client"
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
+import {Button} from "@/components/ui/button";
+import {async} from "rxjs";
+
+const Page = () => {
+
+    const [songUrls, setSongUrls] = useState<string[]>();
+    const [songIds, setSongIds] = useState<string[]>([]);
+
+    const getFavorites = async () => {
+        try {
+            const url = "https://melodio.azurewebsites.net/favorites";
+            const response = await axios.get(url);
+            setSongIds(response.data.map((songId) => songId.id))
+            console.log(response.data.map((songId) => songId.id))
+            let urlArray: string[] = []
+            for (let id of response.data.map((songId) => songId.id)) {
+                const songUrl = `https://open.spotify.com/embed/track/${id}?utm_source=generator`
+                urlArray.push(songUrl);
+            }
+            setSongUrls(urlArray);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getFavorites();
+
+    }, [])
+
+    useEffect(() => {
+        getFavorites();
+    }, [songIds])
+
+    const deleteSongFromPlaylist = async (e: any, songId: string) => {
+        try {
+            const url = `https://melodio.azurewebsites.net/favorites${songId}`;
+            await axios.delete(url);
+            setSongIds(songIds?.filter(id => id !== songId))
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+    return (
+        <div className="grid grid-cols-3 gap-4">
+            {songUrls?.map((songUrl) => (
+                <div key={songUrl} className="w-64">
+                    <iframe
+                        className="rounded-md py-4"
+                        src={songUrl}
+                        width="100%"
+                        height="352"
+                        frameBorder="0"
+                        allowFullScreen={true}
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                        loading="lazy"
+                    ></iframe>
+                    <Button
+                        onClick={(e) => deleteSongFromPlaylist(e, songIds[songUrls?.indexOf(songUrl)])}>Delete</Button>
+
+                </div>
+            ))} </div>
+    )
+}
+export default Page
--- app/something/page.tsx
+++ app/something/page.tsx
@@ -8,10 +8,11 @@ import MapBox from "@/components/MapBox";
 
 const Page = () => {
 
-    const [songIDs, setSongIDs] = useState<string[]>();
+    const [songUrls, setSongUrls] = useState<string[]>();
     const [country, setCountry] = useState('');
     const [longitude, setLongitude] = useState<number>(4.9041);
     const [latitude, setLatitude] = useState<number>(52.3676);
+    const [songIds, setSongIds] = useState<string[]>([]);
 
     const getTopTenTracks = async (e: any) => {
         e.preventDefault();
@@ -20,13 +21,14 @@ const Page = () => {
             const url = `https://melodio.azurewebsites.net/songs/${country}`;
             const response = await axios.get(url);
             const ids: string[] = await response.data;
-            let idsArray: string[] = []
-            for (let url of ids) {
-                const songUrl = `https://open.spotify.com/embed/track/${url}?utm_source=generator`
-                idsArray.push(songUrl);
+            let urlArray: string[] = []
+            for (let id of ids) {
+                const songUrl = `https://open.spotify.com/embed/track/${id}?utm_source=generator`
+                urlArray.push(songUrl);
             }
-            setSongIDs(idsArray);
-            console.log(idsArray);
+            setSongUrls(urlArray);
+            setSongIds(ids);
+            console.log(urlArray);
 
             const urlCoordinates = `https://melodio.azurewebsites.net/coordinates/${country}`;
             const responseCoordinates = await axios.get(urlCoordinates);
@@ -37,20 +39,35 @@ const Page = () => {
             console.error(error);
         }
     }
+    const addSongToPlaylist = async (e: any, songId: string) => {
+        e.preventDefault();
+        const url = "https://melodio.azurewebsites.net/addsong"; // Replace with your API endpoint URL
+        const data = {
+            songId: songId,
+        };
+
+        try {
+            const response = await axios.post(url, data);
+
+            console.log("Response:", response.data);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
 
     return (
         <div className="flex h-screen w-full">
             <div className="w-1/2 p-4 overflow-auto">
-                <Input onChange={(e) => setCountry(e.target.value)} />
+                <Input onChange={(e) => setCountry(e.target.value)}/>
                 <Button className="py-4 my-4" type="submit" onClick={getTopTenTracks}>
                     Add
                 </Button>
                 <div className="grid grid-cols-3 gap-4">
-                    {songIDs?.map((songID) => (
-                        <div key={songID} className="w-64">
+                    {songUrls?.map((songUrl) => (
+                        <div key={songUrl} className="w-64">
                             <iframe
                                 className="rounded-md py-4"
-                                src={songID}
+                                src={songUrl}
                                 width="100%"
                                 height="352"
                                 frameBorder="0"
@@ -58,13 +75,13 @@ const Page = () => {
                                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                                 loading="lazy"
                             ></iframe>
-                            <Button>Add to Cart</Button>
+                            <Button onClick={(e)=> addSongToPlaylist(e,songIds[songUrls?.indexOf(songUrl)])}>Add to Favorite</Button>
                         </div>
                     ))}
                 </div>
             </div>
             <div className="w-1/2">
-                <MapBox  key={`${latitude}-${longitude}`} longitude={longitude} latitude={latitude} />
+                <MapBox key={`${latitude}-${longitude}`} longitude={longitude} latitude={latitude}/>
             </div>
         </div>
     );
