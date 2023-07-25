827f7c6ddefc8fbd669a465f47eef6c8fb5e4031
yoana-tsatsarova <yoana.tsatsarovau@appliedtechnology.se>
2023-07-25 21:13:31 +0200

    getting recommended playlist from spotify

--- app/recommended/recommended.tsx
+++ app/recommended/recommended.tsx
@@ -19,6 +19,8 @@ interface AccountFormProps {
 const Recommended = ({ session }: AccountFormProps) => {
     const [topSongsIds, setTopSongsIds] = useState<string[]>();
     const [recommendedSongsIds, setRecommendedSongsIds] = useState<string[]>();
+    const [allTrackUris, setAllTrackUris] = useState<string[]>();
+    const [playlistUrl, setPlaylistUrl] = useState<string>("");
 
     useEffect(() => {
         const token = session?.provider_token;
@@ -40,37 +42,72 @@ const Recommended = ({ session }: AccountFormProps) => {
             return response.items;
         }
 
-        // const allSongIds: string[] =
-        //
-        // async function getRecommendations(topTracksIds: string[]): Promise<Track[]> {
-        //     const response = await fetchWebApi<{ tracks: Track[] }>(`v1/recommendations?limit=5&seed_tracks=${topTracksIds.join(',')}`, 'GET');
-        //     return response.tracks;
-        // }
-        //
-        //     async function main() {
-        //     try {
-        //         const topTracks = await getTopTracks();
-        //         const topTracksIds: string[] = topTracks.map(track => track.id);
-        //         console.log(topTracksIds)
-        //         setTopSongsIds(topTracksIds);
-        //
-        //         // Get recommended tracks based on top tracks
-        //         const recommendedTracks = await getRecommendations(topTracksIds);
-        //         const recommendedTracksIds: string[] = recommendedTracks.map(track => track.id);
-        //         console.log(recommendedTracksIds);
-        //     } catch (error) {
-        //         console.error('Error fetching data:', error);
-        //     }
-        // }
-        //
-        // main();
+        async function getRecommendations(topTracksIds: string[]): Promise<Track[]> {
+            const response = await fetchWebApi<{ tracks: Track[] }>(`v1/recommendations?limit=5&seed_tracks=${topTracksIds.join(',')}`, 'GET');
+            return response.tracks;
+        }
+
+        async function main() {
+            try {
+                const topTracks = await getTopTracks();
+                const topTracksIds: string[] = topTracks.map(track => track.id);
+                setTopSongsIds(topTracksIds);
+
+                // Get recommended tracks based on top tracks
+                const recommendedTracks = await getRecommendations(topTracksIds);
+                const recommendedTracksIds: string[] = recommendedTracks.map(track => track.id);
+                setRecommendedSongsIds(recommendedTracksIds);
+
+                const allTrackIds: string[] = [...topTracksIds, ...recommendedTracksIds];
+                const trackURIS = allTrackIds.map(id => `spotify:track:${id}`);
+                setAllTrackUris(trackURIS);
+
+                const createdPlaylist = await createPlaylist(trackURIS);
+                const url = `https://open.spotify.com/embed/playlist/${createdPlaylist.id}?utm_source=generator&theme=0`
+                setPlaylistUrl(url);
+
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+
+        async function createPlaylist(tracksUri: string[]){
+            const { id: user_id } = await fetchWebApi('v1/me', 'GET')
+
+            const playlist: Playlist = await fetchWebApi(
+                `v1/users/${user_id}/playlists`, 'POST', {
+                    "name": "Melodio Recommended",
+                    "description": "Playlist created by Melodio based on your listening history",
+                    "public": false
+                })
+
+            await fetchWebApi(
+                `v1/playlists/${playlist.id}/tracks?uris=${tracksUri.join(',')}`,
+                'POST'
+            );
+
+            return playlist;
+        }
+
+        main();
 
 
 
-    }, [session]);
+    }, []);
 
     return (
-        <></>
+        <>
+            <iframe
+                title="Spotify Embed: Recommendation Playlist "
+                src={playlistUrl}
+                width="50%"
+                height="100%"
+                style={{ minHeight: '800px' }}
+                frameBorder="0"
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+            />
+        </>
     )
 }
 
