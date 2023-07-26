9a060168d57b8e33a006c3c1262864d8eac3348d
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2023-07-26 13:25:14 +0200

    Lets get it done

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,10 @@ export default function Home() {
   return (
     <>
       <section>
-          <Login />
+
         <HeroSection />
           <MeetTheTeamSection />
+
       </section>
     </>
   );
