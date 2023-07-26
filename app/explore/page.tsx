4d1d80c43583e82484ada051481d6f3d4294f545
yoana-tsatsarova <yoana.tsatsarovau@appliedtechnology.se>
2023-07-26 09:25:43 +0200

    changes urls

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -61,7 +61,7 @@ const Page = () => {
 
   const addSongToPlaylist = async (e: any, songId: string) => {
     e.preventDefault();
-    const url = "https://melodio.azurewebsites.net/addsong";
+    const url = "https://melodio.azurewebsites.net/songs/add";
     const data = {
       songId: songId,
     };
